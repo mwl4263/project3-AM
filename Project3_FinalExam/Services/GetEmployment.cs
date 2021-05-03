027f12a0ecf3606eee1324b0e2e81bc209ff937f
aag7593 <aag7593@rit.edu>
2021-05-03 16:55:07 -0400

    initial project

---
+++ Project3_FinalExam/Services/GetEmployment.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project3_FinalExam.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Project3_FinalExam.Services
+{
+    public class GetEmployment
+    {
+        public async Task<List<ProfessionalEmploymentInfoEntries>> GetEmploymentData()
+        {
+            using (var client1 = new HttpClient())
+            {
+                client1.BaseAddress = new Uri("http://www.ist.rit.edu/");
+                client1.DefaultRequestHeaders.Accept.Clear();
+                client1.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                try
+                {
+                    HttpResponseMessage response = await client1.GetAsync("api/employment", HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+                    var data = await response.Content.ReadAsStringAsync();
+
+                    // Above three lines can be replaced with new helper method below
+                    // string responseBody = await client.GetStringAsync(uri);
+
+                    var rtnResults = JsonConvert.DeserializeObject<Dictionary<string, List<ProfessionalEmploymentInfoEntries>>>(data);
+                    List<ProfessionalEmploymentInfoEntries> employmentList = new List<ProfessionalEmploymentInfoEntries>();
+                    ProfessionalEmploymentInfoEntries employmentData = new ProfessionalEmploymentInfoEntries();
+
+                    foreach (KeyValuePair<string, List<ProfessionalEmploymentInfoEntries>> kvp in rtnResults)
+                    {
+                        foreach (var item in kvp.Value)
+                        {
+                            employmentList.Add(item);
+                        }
+                    }
+
+                    return employmentList;
+
+
+
+                }
+                catch (HttpRequestException hre)
+                {
+                    var msg = hre.Message;
+                    List<ProfessionalEmploymentInfoEntries> employmentList = new List<ProfessionalEmploymentInfoEntries>();
+                    return employmentList;
+                }
+                catch (Exception ex)
+                {
+                    var msg = ex.Message;
+                    List<ProfessionalEmploymentInfoEntries> employmentList = new List<ProfessionalEmploymentInfoEntries>();
+                    return employmentList;
+                }
+            }
+        }
+    }
+}
---
+++ Project3_FinalExam/ViewModels/EmploymentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project3_FinalExam.Models;
+
+namespace Project3_FinalExam.ViewModels
+{
+    public class EmploymentViewModel
+    {
+        public List<ProfessionalEmploymentInfoEntries> employment { get; set; }
+        public string Title { get; set; }
+
+    }
+}
