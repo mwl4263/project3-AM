3c50016093fb55f1ede53d877642cc016e739899
aag7593 <aag7593@rit.edu>
2021-05-05 18:47:31 -0400

    moving jsgrid
    lknsadln

---
+++ Project3_FinalExam/Models/coopInfoEntries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Project3_FinalExam.Models
+{
+    public class coopInfoEntries
+    {
+        public string employer { get; set; }
+        public string degree { get; set; }
+        public string city { get; set; }
+        public string term { get; set; }
+    }
+}
