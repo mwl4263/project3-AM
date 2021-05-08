7d71401ad145b4e8e3c4825efa252606aaa2ff37
mwl4263 <masonlapine@acd4122904532376873289.wireless.rit.edu>
2021-05-08 14:41:15 -0400

    Trouble with about

---
+++ Project3_FinalExam/ViewModels/AboutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project3_FinalExam.Models;
+
+namespace Project3_FinalExam.ViewModels
+{
+    public class AboutViewModel
+    {
+        public List<About> aboutInfo { get; set; }
+        public string title { get; set; }
+
+    }
+}
